100e4093e1b86e83d4beb30b49cc017f79ba6705
fedealexsium <trentin.federico@libero.it>
2023-04-01 20:09:05 +0200

    commit 2 verde

--- Test.cs
+++ Test.cs
@@ -5,7 +5,7 @@ public static class Test
 {
     public static void Main(string[] args)
     {
-        string expected = "sky";
+        string expected = "msky";
         string returned = Applicazione.Es1("ciao", 10);
         Assert.Equal(expected, returned);
     }
