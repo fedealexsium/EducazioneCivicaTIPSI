a9aa2be64c354256ee380837adf5df0b71d2e4cd
fedealexsium <trentin.federico@libero.it>
2023-04-01 21:01:17 +0200

    Revert "commit 3 rosso"
    This reverts commit 62ad92e862665485d006fafc629f442d124a0b60.

--- Program.cs
+++ Program.cs
@@ -24,26 +24,4 @@ public static class Applicazione
         }
         return stringaFinale;
     }
-
-    public static string Es2(string s, int n)
-    {
-        int index = 0;
-        string stringaFinale = "";
-        foreach (var item in s)
-        {
-            s = s.ToLower();
-            index = 0;
-            while (item != alfabeto[index])
-            {
-                index++;
-            }
-            index -= n;
-            if (index < 0)
-            {
-                index += 25;
-            }
-            stringaFinale += alfabeto[index];
-        }
-        return stringaFinale;
-    }
 }
--- Test.cs
+++ Test.cs
@@ -5,14 +5,8 @@ public static class Test
 {
     public static void Main(string[] args)
     {
-        //es1
         string expected = "msky";
         string returned = Applicazione.Es1("ciao", 10);
-        Assert.Equal(expected, returned);
-
-        //es2
-        expected = "ciao";
-        returned = Applicazione.Es2("msky", 10);
         Assert.Equal(expected, returned);
     }
 }
