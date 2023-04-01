b24dab61f5feae311eff06398fcc09a621e436fc
fedealexsium <trentin.federico@libero.it>
2023-04-01 20:07:56 +0200

    commit 1 rosso

--- Program.cs
+++ Program.cs
@@ -2,8 +2,26 @@
 
 public static class Applicazione
 {
-    public static string Es1(string s)
+    private static string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    public static string Es1(string s, int n)
     {
-        return "";
+        int index = 0;
+        string stringaFinale = "";
+        foreach (var item in s)
+        {
+            s = s.ToLower();
+            index = 0;
+            while (item != alfabeto[index])
+            {
+                index++;
+            }
+            index += n;
+            if (index > 25)
+            {
+                index -= 25;
+            }
+            stringaFinale += alfabeto[index];
+        }
+        return stringaFinale;
     }
 }
--- Test.cs
+++ Test.cs
@@ -1,9 +1,12 @@
 using System;
+using Xunit;
 
 public static class Test
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(Applicazione.Es1("ciao"));
+        string expected = "sky";
+        string returned = Applicazione.Es1("ciao", 10);
+        Assert.Equal(expected, returned);
     }
 }
