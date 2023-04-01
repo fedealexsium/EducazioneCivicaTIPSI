2c8b3e6b8bdcf2b8c4d8b40b722eaea6a851e64c
fedealexsium <trentin.federico@libero.it>
2023-04-01 19:32:12 +0200

    commit base

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class Applicazione
+{
+    public static string Es1(string s)
+    {
+        return "";
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,9 @@
+using System;
+
+public static class Test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Applicazione.Es1("ciao"));
+    }
+}
